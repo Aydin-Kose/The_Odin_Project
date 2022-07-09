6269f6d3af3c6559161a4c0e64b84bea4d365064
Aydin-Kose <red---@outlook.com>
2022-07-09 17:07:43 +0300

    continue examples

--- 7.Javascript-exercises/08_calculator/calculator.js
+++ 7.Javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((total, arg) =>{
+    return total + arg;
+  },0);
 };
 
 const subtract = function() {
-	
+	return arguments[0]- arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, arg) => total + arg,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,num) =>{
+    return total*num;
+  },1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a===0) {
+    return 1;
+  }
+  return factorial(a-1)*a;
 };
 
 // Do not edit below this line
