c45dd10f38f215db3347e6bcee62b55076d99e6d
Aydin-Kose <red---@outlook.com>
2022-07-10 00:42:10 +0300

    examples

--- 7.Javascript-exercises/12_findTheOldest/findTheOldest.js
+++ 7.Javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((first,last) => (getDeathYear(first.yearOfDeath)-first.yearOfBirth) < (getDeathYear(last.yearOfDeath)-last.yearOfBirth)?1:-1)[0];
 };
+
+function getDeathYear(yearOfDeath) {
+    console.log(yearOfDeath?yearOfDeath:new Date().getFullYear());
+    return yearOfDeath?yearOfDeath:new Date().getFullYear();
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
