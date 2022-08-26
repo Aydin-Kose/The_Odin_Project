88469cbce25b1cc62afb7393ad2129e13cc751dc
Aydin-Kose <red---@outlook.com>
2022-08-27 01:36:19 +0300

    Create GameBoard class

--- 17.TicTacToe/script.js
+++ 17.TicTacToe/script.js
@@ -1,11 +1,39 @@
+const Player = (() => {
+    let Value = 'X';
+    return { Value };
+})();
+
 const GameBoard = (() => {
-    let cell = (x, y) => {
-        let value = '';
-        return { x, y, value }
+    const board = [['', '', ''], ['', '', ''], ['', '', '']];
+    const cells = document.querySelectorAll('.cell');
+    const checkWin = (row, column) => {
+        if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
+            return true;
+        }
+        else if (board[0][column] === board[1][column] && board[0][column] === board[2][column]) {
+            return true;
+        }
+        else if (board[1][1] !== '' && 
+        ((board[0][0] === board[1][1] && board[1][1] === board[2][2])||
+        board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
+            return true;
+        }
+        return false;
     };
-    const board = [cell(0, 0), cell(0, 1), cell(0, 3),
-    cell(1, 0), cell(1, 1), cell(1, 3),
-    cell(2, 0), cell(2, 1), cell(2, 3),];
-    return { board };
+    const play = function (cell, player) {
+        let row = cell.dataset.row;
+        let column = cell.dataset.column;
+        board[row][column] = player.Value;
+        cell.textContent = player.Value;
+        console.log(checkWin(row, column));
+    };
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('click', play.bind(this, cells[i], Player), false);
+    }
+    return { board, play };
 })();
 console.log(GameBoard.board);
+
+
+
+
