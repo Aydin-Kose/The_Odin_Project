0feee9578780810aaeb45021fa244285cbfea848
Aydin-Kose <red---@outlook.com>
2022-08-27 16:32:03 +0300

    random playing ai created

--- 17.TicTacToe/script.js
+++ 17.TicTacToe/script.js
@@ -1,11 +1,32 @@
-const Player = (() => {
-    let Value = 'X';
+const Player = ((value = 'X') => {
+    let Value = value;
     return { Value };
-})();
+});
 
-const GameBoard = (() => {
+const PcPlayer = (difficulty) => {
+    let makeMove;
+    if (difficulty == 'easy') {
+        makeMove = (board) => {
+            let emptyBoardIndex = [];
+            for (let i = 0; i < board.length; i++) {
+                const row = board[i];
+                for (let j = 0; j < row.length; j++) {
+                    const cell = row[j];
+                    if (cell === '') {
+                        emptyBoardIndex.push([i, j])
+                    }
+                }
+            }
+            let randomCellIndex = emptyBoardIndex[Math.floor(Math.random() * emptyBoardIndex.length)];
+            let cell = document.querySelector(`[data-row='${randomCellIndex[0]}'][data-column='${randomCellIndex[1]}']`);
+            return cell;
+        }
+    }
+    return { makeMove };
+};
+
+const GameBoard = () => {
     const board = [['', '', ''], ['', '', ''], ['', '', '']];
-    const cells = document.querySelectorAll('.cell');
     const checkWin = (row, column) => {
         if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
             return true;
@@ -13,27 +34,43 @@ const GameBoard = (() => {
         else if (board[0][column] === board[1][column] && board[0][column] === board[2][column]) {
             return true;
         }
-        else if (board[1][1] !== '' && 
-        ((board[0][0] === board[1][1] && board[1][1] === board[2][2])||
-        board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
+        else if (board[1][1] !== '' &&
+            ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+                board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
             return true;
         }
         return false;
     };
-    const play = function (cell, player) {
+    const endGame = function () {
+
+    }
+    const play = function (cell, player, pcPlayer) {
         let row = cell.dataset.row;
         let column = cell.dataset.column;
-        board[row][column] = player.Value;
-        cell.textContent = player.Value;
-        console.log(checkWin(row, column));
+        if (board[row][column] === '') {
+            board[row][column] = player.Value;
+            cell.textContent = player.Value;
+            if (checkWin(row, column)) {
+                endGame();
+            }
+            else if (pcPlayer !== undefined) {
+                debugger;
+                play(pcPlayer.makeMove(board), pcPlayer);
+            }
+        }
     };
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('click', play.bind(this, cells[i], Player), false);
-    }
+
     return { board, play };
+};
+
+const Game = (() => {
+    const player = Player('X');
+    const pcPlayer = Player('O');
+    pcPlayer.prototype = Object.assign(pcPlayer, PcPlayer('easy'));
+    let gameBoard = GameBoard();
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('click', gameBoard.play.bind(gameBoard, cells[i], player, pcPlayer), false);
+    }
+    return { gameBoard, player };
 })();
-console.log(GameBoard.board);
-
-
-
-
