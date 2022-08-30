8aeb5885c886839c06f9558cad971ffeb1d4b400
Aydin-Kose <red---@outlook.com>
2022-08-30 21:15:39 +0300

    WebPack Example v1

---
+++ 18.Node-Webpack/src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = myName('Red')
+    return element;
+}
+
+document.body.appendChild(component());
