37e1406ff441dd3bc666a19c2426f7cd7e9084e7
Aydin-Kose <red---@outlook.com>
2022-08-31 00:05:17 +0300

    Webpack

--- 18.Node-Webpack/src/index.js
+++ 18.Node-Webpack/src/index.js
@@ -1,11 +1,25 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css'
+import webpack from './webpack.svg'
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = myName('Red')
+    element.classList.add('hello');
+
+    const myImage = new Image();
+    myImage.src = webpack;
+
+    element.appendChild(myImage);
+
     return element;
 }
 
 document.body.appendChild(component());
+
+console.log(Data);
+console.log(Notes);
